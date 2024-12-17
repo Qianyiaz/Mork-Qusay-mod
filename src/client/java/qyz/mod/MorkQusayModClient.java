0661ba8e46968af6ad0be66d0ce3d67d9aa5f58b
Qusay Diaz <Qusay_Diaz.github@outlook.com>
2024-12-17 21:28:41 +0800

    项目管理：初始化。

---
+++ src/client/java/qyz/mod/MorkQusayModClient.java
@@ -0,0 +1,10 @@
+package qyz.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MorkQusayModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
